40d7ccf769607953998eb5338ff964f8fbca3b4f
C E Z A#6466 <93098721+cezacs1@users.noreply.github.com>
2023-11-15 03:55:22 +0300

    Add files via upload

---
+++ vac_runfunc.dll/vac_runfunc_dll/Imports.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runfunc_dll
+{
+    public class Imports
+    {
+        public const uint GENERIC_READ = 0x80000000;
+        public const uint OPEN_EXISTING = 3;
+        public const uint PAGE_READONLY = 0x02;
+        public const uint FILE_MAP_READ = 0x04;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr CreateFile(string lpFileName, uint dwDesiredAccess, uint dwShareMode, IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr CreateFileMapping(IntPtr hFile, IntPtr lpFileMappingAttributes, uint flProtect, uint dwMaximumSizeHigh, uint dwMaximumSizeLow, string lpName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr MapViewOfFile(IntPtr hFileMappingObject, uint dwDesiredAccess, uint dwFileOffsetHigh, uint dwFileOffsetLow, UIntPtr dwNumberOfBytesToMap);    
+    }
+}
---
+++ vac_runfunc.dll/vac_runfunc_dll/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using static runfunc_dll.Imports;
+
+namespace runfunc_dll
+{
+    class Program
+    {
+        private static void Main()
+        {
+            // Dosya yolunu ve bellek eşlemesi için dosya adını belirle
+            string filePath = "C:\\Users\\ceza\\source\\repos\\ConsoleApp3\\private\\bin\\Debug\\1.exe";
+            string mappingObjectName = "Global\\MyFileMappingObject";
+
+            // Dosyayı aç
+            IntPtr hFile = CreateFile(filePath, GENERIC_READ, 0, IntPtr.Zero, OPEN_EXISTING, 0, IntPtr.Zero);
+
+            if (hFile != IntPtr.Zero)
+            {
+                // Dosya eşlemesi oluştur
+                IntPtr hMapping = CreateFileMapping(hFile, IntPtr.Zero, PAGE_READONLY, 0, 1, mappingObjectName);
+
+                if (hMapping != IntPtr.Zero)
+                {
+                    // Memory map'i al
+                    IntPtr pData = MapViewOfFile(hMapping, FILE_MAP_READ, 0, 0, UIntPtr.Zero);
+
+                    if (pData != IntPtr.Zero)
+                    {
+                        //long size = GetFileSize(pData);
+
+                        byte[] buffer = new byte[/*size*/999]; // Örnek bir tampon
+                        Marshal.Copy(pData, buffer, 0, buffer.Length);
+                        string content = Encoding.UTF8.GetString(buffer);
+                        Console.WriteLine("buffer (string): " + content);
+
+                        Console.WriteLine("buffer (hex):\n");
+                        foreach (byte b in buffer)
+                        {
+                            Console.Write($"{b:X2} ");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error from MapViewOfFile -> " + Marshal.GetLastWin32Error());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error from CreateFileMapping -> " + Marshal.GetLastWin32Error());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error from CreateFile -> " + Marshal.GetLastWin32Error());
+            }
+
+            Console.ReadKey();
+        }
+
+        /* //test
+        static long GetFileSize(IntPtr pData)
+        {
+            string filePath = Marshal.PtrToStringAnsi(pData);
+            if (File.Exists(filePath))
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                return fileInfo.Length;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        */
+    }
+}
