070104c58cf7b423faab395ae92b32af777ba4c3
C E Z A#6466 <93098721+cezacs1@users.noreply.github.com>
2023-11-15 19:44:14 +0300

    Add files via upload

---
+++ vac_some_retaddr_1/some_retaddr_1/Engine2Imports.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class Engine2Imports
+{
+    [DllImport("engine2.dll")]
+    public static extern Int64 LoggingSystem_IsChannelEnabled(ulong param1, ulong param2);
+
+    [DllImport("engine2.dll")]
+    public static extern Int64 LOG_CONSOLE()/* = 0*/;
+
+    [DllImport("engine2.dll")]
+    public static extern long LoggingSystem_Log(ulong param1, ulong param2, string format, params object[] args);
+}
---
+++ vac_some_retaddr_1/some_retaddr_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("some_retaddr")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("some_retaddr")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("5e883612-aff4-4440-83c6-2fb63d7b10d7")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
